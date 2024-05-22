48c3315aac91c689b915beff8d3c524535ce9675
my_user <oamenmodupe@gmail.com>
2024-05-22 14:34:57 +0100

    Edited yml workflow

--- develop/data_retrieval.py
+++ develop/data_retrieval.py
@@ -4,5 +4,21 @@ import pandas as pd
 import numpy as np
 import boto3
 import os
+import json
 
-print(os.getenv('AWS_SECRET_ACCESS_KEY'))
+access_key = os.getenv('AWS_ACCESS_KEY')
+access_key_id = os.getenv('AWS_ACCESS_KEY')
+
+# create S3 client object 
+s3_client = boto3.client('s3',aws_access_key_id=access_key_id,aws_secret_access_key=access_key)
+
+config_path = "./config.json"
+
+# Opening JSON file
+with open(config_path) as file:
+    
+    config = json.load(file)
+
+bucket_name = config['EDA']['bucket_name'] 
+
+test_df = s3_retrieval(s3_client, bucket_name, config['Model_Selection']['Input_data']['test'])
