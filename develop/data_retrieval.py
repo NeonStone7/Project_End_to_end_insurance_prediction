ac9f68b8d12f0984556f74585ea89155865b1e81
my_user <oamenmodupe@gmail.com>
2024-05-22 11:25:20 +0100

    Added model_selection and workflow for secrets

---
+++ develop/data_retrieval.py
@@ -0,0 +1,8 @@
+from package.data_retrieval.data_retrieval import s3_retrieval, write_to_s3
+from package.training.model_training import model_evaluation, split_data_by_type, performance_plots
+import pandas as pd
+import numpy as np
+import boto3
+import os
+
+print(os.getenv('AWS_ACCESS_KEY'))
