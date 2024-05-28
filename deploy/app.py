4d5fcdf7d641ae498b05b464c18623d898e33a72
my_user <oamenmodupe@gmail.com>
2024-05-28 18:05:24 +0100

    Added airflow, created docker image

--- deploy/app.py
+++ deploy/app.py
@@ -1,8 +1,9 @@
-"""Flask app to expose model to users"""
 import json
 import pandas as pd
 from flask import Flask, render_template, request, send_file
 import mlflow
+from sklearn import set_config
+set_config(transform_output = 'pandas')
 
 app = Flask(__name__)
 
@@ -12,56 +13,63 @@ with open(CONFIG_PATH, 'r') as config_file:
     config = json.load(config_file)
 
 # extract required variables
-experiment_name = config['Develop']['Preprocessor']['experiment_name'] 
-run_name = config['Develop']['Model']['run_name'] 
+experiment_name = config['Develop']['Preprocessor']['experiment_name']
+run_name = config['Develop']['Model']['run_name']
 model_name = config['Develop']['Model']['model_name']
 model_artifact_path = config['Develop']['Model']['artifact_path']
-model_run_id =  config['Develop']['Model']['run_id'] 
+model_run_id = config['Develop']['Model']['run_id']
 preprocessor_run_id = config['Develop']['Preprocessor']['run_id']
 preprocessor_artifact_path = config['Develop']['Preprocessor']['artifact_path']
-selected_columns = config['Develop']['Model']['selected_columns'] 
+selected_columns = config['Develop']['Model']['selected_columns']
 
-# load model and preprocessor
+# Load model and preprocessor only once
 preprocessor_uri = f"runs:/{preprocessor_run_id}/{preprocessor_artifact_path}"
 preprocessor_model = mlflow.pyfunc.load_model(preprocessor_uri)
 
 model_uri = f'runs:/{model_run_id}/{model_artifact_path}'
-model = mlflow.sklearn.load_model(model_uri = model_uri)
+model = mlflow.sklearn.load_model(model_uri)
+
 
 @app.route('/')
 def home():
+    return render_template('home.html')
 
-    return render_template('./home.html')
 
-@app.route('/predict', methods = ['GET', 'POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    
-    if request.method == 'POST':
-        file = request.file['file']
-        if file:
+    # bad practice
+    from sklearn import set_config
+    set_config(transform_output = 'pandas')
 
+    if request.method == 'POST':
+        file = request.files.get('file')  # Use request.files.get to safely get the file
+        if file:
             # read the csv file
             input_df = pd.read_csv(file)
 
             # preprocess the data
             input_df_processed = preprocessor_model.predict(input_df)
-
             input_df_processed = input_df_processed[selected_columns]
 
             # get predictions and add to file
             input_df['Prediction'] = model.predict(input_df_processed)
 
             # save the csv
-            output_path = 'output_data.csv'
-            input_df.to_csv(output_path, index = False)
+            save_output_path = './deploy/output_data.csv'
+            input_df.to_csv(save_output_path, index=False)
 
+            file_name = 'output_data.csv'
             # return the csv
-            return send_file(output_path, as_attachment = True)
-        
-@app.route('/monitor')
-def monitor():
+            return send_file(file_name, as_attachment=True)
+        else:
+            return render_template('predict.html', message="No file provided")
+    return render_template('predict.html')  # Render the file upload form
 
-    return render_template('./data_and_target_drift_dashboard.html')
+
+# @app.route('/monitor')
+# def monitor():
+#     return render_template('data_and_target_drift_dashboard.html')
+
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True, host='0.0.0.0', port=3000)
--- test_file.py
+++ test_file.py
@@ -1,12 +1,45 @@
-from package.data_retrieval.data_retrieval import s3_retrieval, write_to_s3
-from package.eda.data_exploration import correlation
-from sklearn.datasets import load_iris
+import json
 import pandas as pd
+from flask import Flask, render_template, request, send_file
+import mlflow
+from sklearn import set_config
+set_config(transform_output = 'pandas')
 
-data = pd.DataFrame(load_iris().data, columns = load_iris().feature_names)
+app = Flask(__name__)
 
+# open config
+CONFIG_PATH = './config.json'
+with open(CONFIG_PATH, 'r') as config_file:
+    config = json.load(config_file)
 
-correlation(data,
-    method = 'pearson',
-    figsize = (20,20),
-    annot = True)
+# extract required variables
+experiment_name = config['Develop']['Preprocessor']['experiment_name']
+run_name = config['Develop']['Model']['run_name']
+model_name = config['Develop']['Model']['model_name']
+model_artifact_path = config['Develop']['Model']['artifact_path']
+model_run_id = config['Develop']['Model']['run_id']
+preprocessor_run_id = config['Develop']['Preprocessor']['run_id']
+preprocessor_artifact_path = config['Develop']['Preprocessor']['artifact_path']
+selected_columns = config['Develop']['Model']['selected_columns']
+
+# print(selected_columns)
+
+# # Load model and preprocessor only once
+# preprocessor_uri = f"runs:/{preprocessor_run_id}/{preprocessor_artifact_path}"
+# preprocessor_model = mlflow.pyfunc.load_model(preprocessor_uri)
+
+# model_uri = f'runs:/{model_run_id}/{model_artifact_path}'
+# model = mlflow.sklearn.load_model(model_uri)
+
+input_df = pd.read_csv("C:/Users/Oamen/OneDrive/Documents/DATASETS/porto_seguro_insurance_data/porto-seguro-safe-driver-prediction/test.csv")
+
+# # preprocess the data
+# input_df_processed = preprocessor_model.predict(input_df)
+# print(input_df_processed[:2])
+# input_df_processed = input_df_processed[selected_columns]
+
+# # get predictions and add to file
+# input_df['Prediction'] = model.predict(input_df_processed)
+
+# take asample
+input_df.sample(2000).to_csv('test_data.csv', index = False)
